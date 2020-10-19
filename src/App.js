c2d591f9276a4382587a060014fc9c987c03d889
Ciprian Pîțul <clfxc@192-168-0-175.rdsnet.ro>
2020-10-19 17:29:43 +0300

    changes to directory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 import './css/mediaQueriesWeb.css'
 import './css/mediaQueriesMobile.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
